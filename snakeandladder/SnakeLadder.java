edbe78d37e8be6e7b6d82aa5ba20e5d94dfff7ca
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-25 18:09:25 +0530

    Added: Player roll the Die

--- snakeandladder/SnakeLadder.java
+++ snakeandladder/SnakeLadder.java
@@ -4,5 +4,7 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
 		System.out.println("Player is at 0 Position");
+		int dice = (int) Math.floor(Math.random() * 6 + 1);
+		System.out.println("The number on dice is " + dice);
 	}
 }
