045f31f7a319d14a125f1afb86c7bcd65e8a5439
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-25 17:57:58 +0530

    Added: Started With single player at 0 position

--- snakeandladder/SnakeLadder.java
+++ snakeandladder/SnakeLadder.java
@@ -3,5 +3,6 @@ package snakeandladder;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
+		System.out.println("Player is at 0 Position");
 	}
 }
