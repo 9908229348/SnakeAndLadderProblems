cdb5c94083506684cf3d16530bc3b8f2010c5f56
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-25 18:21:14 +0530

    Added: Chech for option the Player got

--- snakeandladder/SnakeLadder.java
+++ snakeandladder/SnakeLadder.java
@@ -3,8 +3,30 @@ package snakeandladder;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
-		System.out.println("Player is at 0 Position");
 		int dice = (int) Math.floor(Math.random() * 6 + 1);
 		System.out.println("The number on dice is " + dice);
+		int option = (int) Math.floor(Math.random() * 3);
+		//int noPlay = 0;
+		int ladder = 1;
+		int snake = 2;
+		int urPresentStep = 10;
+		System.out.println("Present You are on Step:" + urPresentStep);
+		System.out.println("If you get option as 0 it is noPlay,1 it is ladder,2 it is snake " );
+		System.out.println("You got option" + option);
+		if(option == ladder)
+		{
+			urPresentStep += dice;
+			System.out.println("You are on :" + urPresentStep);
+		}
+			else if(option == snake)
+			{
+				urPresentStep -= dice;
+				System.out.println("You are on : " + urPresentStep);
+			}
+			else 
+			{
+				System.out.println("You are on :" + urPresentStep);
+			}
+		
 	}
 }
