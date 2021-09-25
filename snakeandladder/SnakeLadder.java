657af7f3b2d6f763c7c73b628c8883d1ed3b74a1
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-25 19:08:44 +0530

    Added: Ensure that Player gets to exact winning position100

--- snakeandladder/SnakeLadder.java
+++ snakeandladder/SnakeLadder.java
@@ -7,30 +7,30 @@ public class SnakeLadder {
 		int ladder = 1;
 		int snake = 2;
 		int urPresentStep = 0;
-		System.out.println("Starting the Game and You Are On step:" + urPresentStep);
 		while(urPresentStep < 100)
 		{
 		int dice = (int) Math.floor(Math.random() * 6 + 1);
 		System.out.println("The number on dice is " + dice);
 		int option = (int) Math.floor(Math.random() * 3);	
-		System.out.println("If you get option as 0 it is noPlay,1 it is ladder,2 it is snake " );
-		System.out.println("You got option" + option);
+		System.out.println("If you get 0 it is noPlay,1 it is ladder,2 it is snake " );
+		System.out.println("You got " + option);
 		if(option == ladder)
 		{
 			urPresentStep += dice;
-			System.out.println("You are on step:" + urPresentStep);
+			if(urPresentStep > 100)
+				urPresentStep -= dice;
+			System.out.println("You are on :" + urPresentStep);
 		}
 			else if(option == snake)
 			{
 				urPresentStep -= dice;
 				if(urPresentStep < 0)
 					urPresentStep = 0;
-				System.out.println("You are on step : " + urPresentStep);
-				
+				System.out.println("You are on : " + urPresentStep);			
 			}
 			else 
 			{
-				System.out.println("You are on step :" + urPresentStep);
+				System.out.println("You are on :" + urPresentStep);
 			}
 		
 	}
