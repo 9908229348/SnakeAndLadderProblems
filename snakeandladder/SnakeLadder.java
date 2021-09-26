cb56134eee48a2d7af4c6f2598aa7eb4d9acacff
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-26 19:51:42 +0530

    Added: Snake and Ladder Game for Two Players

--- snakeandladder/SnakeLadder.java
+++ snakeandladder/SnakeLadder.java
@@ -1,41 +1,57 @@
 package snakeandladder;
 
 public class SnakeLadder {
-	public static void main(String[] args) {
-		System.out.println("Starting of Snake and Ladder Game");
-		//int noPlay = 0;
-		int ladder = 1;
-		int snake = 2;
+	int forOptions() {
+		int options = (int)Math.floor(Math.random() * 3);
+		System.out.println("If you get option 0 it is noPlay,1 it is ladder,2 it is snake ");
+		System.out.println("You got option :" + options);
+		return options;
+	}
+		int play() {
 		int urPresentStep = 0;
-		int dieRoll = 0;
-		while(urPresentStep < 100)
-		{
-		dieRoll++;
-		int dice = (int) Math.floor(Math.random() * 6 + 1);
-		System.out.println("The number on dice is " + dice);
-		int option = (int) Math.floor(Math.random() * 3);	
-		System.out.println("If you get 0 it is noPlay,1 it is ladder,2 it is snake " );
-		System.out.println("You got option " + option);
-		if(option == ladder)
-		{
-			urPresentStep += dice;
-			if(urPresentStep > 100)
-				urPresentStep -= dice;
-			System.out.println("You are on step :" + urPresentStep);
-		}
-			else if(option == snake)
+		int noOfTimesdieRolled = 0;
+		//int ladder = 1;
+		//int snake = 2;
+		while(urPresentStep < 100) {
+			noOfTimesdieRolled++;
+			SnakeLadder playing = new SnakeLadder();
+			int dieRoll = (int)Math.floor(Math.random() * 6 + 1);
+			System.out.println("The Number on dice: " + dieRoll);
+			switch(playing.forOptions())
 			{
-				urPresentStep -= dice;
+			case 1:
+				urPresentStep += dieRoll;
+				if(urPresentStep > 100)
+					urPresentStep -= dieRoll;
+				System.out.println("You are on step:" + urPresentStep);
+				int extraRoll = (int)Math.floor(Math.random() * 6 + 1);
+				System.out.println("The Extra Roll for getting Ladder and you get Number :" + extraRoll);
+				urPresentStep += extraRoll;
+				if(urPresentStep > 100)
+					urPresentStep -= extraRoll;
+				System.out.println("You are on step:" + urPresentStep);
+				break;
+			case 2:
+				urPresentStep -= dieRoll;
 				if(urPresentStep < 0)
 					urPresentStep = 0;
-				System.out.println("You are on step : " + urPresentStep);				
+				System.out.println("You are on step:" + urPresentStep);
+				break;
+			default:
+				System.out.println("You are on Step :" + urPresentStep);
 			}
-			else 
-			{
-				System.out.println("You are on step :" + urPresentStep);
-			}
-	}
-		System.out.println("Number of times die rolled to win the game : " + dieRoll);
+		}
+			return noOfTimesdieRolled;
+		}
+		public static void main(String[] args) {
+			SnakeLadder game = new SnakeLadder();
+		    int playerOne = game.play();
+		    int playerTwo = game.play();
+		    System.out.println("No.of Times Dice rolled by playerOne to Reach 100: " + playerOne);
+		    System.out.println("No.of Times Dice rolled by playerTwo to Reach 100: " + playerTwo);
+		    if(playerOne < playerTwo)
+		    	System.out.println("playerOne is winner");
+		    else
+		    	System.out.println("playerTwo is winner");
+		}
 }
-}
-
